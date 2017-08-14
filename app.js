61278aba745f52f571aa217ae21edc35e284e6ae
Shireen Garcia <garciasal@gmail.com>
2017-08-14 16:21:26 -0700

    Added more stuff cannot quite rememberwhat

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const mustacheExpress = require('mustache-express');
+const mustacheExpress = require('mustache-express')
+var jsonfile = require('jsonfile')
+var file = '/tmp/data.json'
+jsonfile.readFile(file, function(err, obj) {
+  console.dir(obj)
+})
+
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
@@ -9,8 +15,16 @@ app.set('view engine', 'mustache')
 
 app.use(express.static(path.join(__dirname, 'static')))
 
+const todos = [];
+let taskId = 0;
+
 app.get("/", function(req, res, next){
-  res.render("index", {appType:"Express"})
+  res.render("index", { todos: todos });
+});
+
+app.post("/", function (req, res) {
+  todos.push(req.body.listtask);
+  res.redirect('/');
 })
 
 app.listen(3000, function(){
